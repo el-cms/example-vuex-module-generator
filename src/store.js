315bc5a80956eacb0fe1c9fd079d32ee438d8a12
Manuel Tancoigne <m.tancoigne@gmail.com>
2018-09-27 16:22:56 +0200

    Finish part3 - customization

--- src/store.js
+++ src/store.js
@@ -12,5 +12,19 @@ export default new Vuex.Store({
     users: Modulator.generateModule('users', 'user', 'users', true, false),
     posts: Modulator.generateModule('posts', 'post', 'posts'),
     postTypes: Modulator.generateModule('post_types', 'post_type', 'post_types'),
-  }
+    comments: {
+      state: {},
+      mutations: {
+        ...Modulator.mutations('comment'),
+        my_custom_mutation: (state, data) => {/* do something */},
+      },
+      actions: {
+        ...Modulator.actions('comments', 'comment', 'comments'),
+        createComment: ({commit}, payload) => {/* override an action from the Modulator */},
+      },
+      getters: {
+        ...Modulator.getters('comment', 'comments'),
+      },
+    },
+  },
 })
