a2267462a7c528bfc7ad1a0d8e4155ac2ce261ee
Manuel Tancoigne <m.tancoigne@gmail.com>
2018-09-28 10:38:41 +0200

    Update store to use module present in fake data

--- src/store.js
+++ src/store.js
@@ -10,21 +10,9 @@ export default new Vuex.Store({
   actions: {},
   modules: {
     users: Modulator.generateModule('users', 'user', 'users', true, false),
-    posts: Modulator.generateModule('posts', 'post', 'posts'),
-    postTypes: Modulator.generateModule('post_types', 'post_type', 'post_types'),
-    comments: {
-      state: {},
-      mutations: {
-        ...Modulator.mutations('comment'),
-        my_custom_mutation: (state, data) => {/* do something */},
-      },
-      actions: {
-        ...Modulator.actions('comments', 'comment', 'comments'),
-        createComment: ({commit}, payload) => {/* override an action from the Modulator */},
-      },
-      getters: {
-        ...Modulator.getters('comment', 'comments'),
-      },
-    },
+    addresses: Modulator.generateModule('addresses', 'address', 'addresses'),
+    articles: Modulator.generateModule('articles', 'article', 'articles'),
+    tags: Modulator.generateModule('tags', 'tag', 'tags'),
+    tasks: Modulator.generateModule('tasks', 'task', 'tasks'),
   },
 })
